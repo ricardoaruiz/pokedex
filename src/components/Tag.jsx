a72cf388549ff249c2d98ca34a2972abafa22306
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2025-06-01 01:12:58 -0300

    feat: tag

--- src/components/Tag.jsx
+++ src/components/Tag.jsx
@@ -2,7 +2,7 @@
  * Esse componente é responsável por renderizar uma tag estilizada.
  * Ele recebe um prop `children` que representa o conteúdo a ser exibido dentro da tag.
  * A tag é estilizada com classes do Tailwind CSS para ter um fundo escuro e bordas arredondadas.
- * @param {Object} param0 - Propriedades do componente.
+ * @param {Object} param0 - Propriedades do componente
  * @param {React.ReactNode} param0.children - O conteúdo a ser exibido dentro da tag.
  * @returns {JSX.Element} - Retorna uma div estilizada com o conteúdo fornecido.
  */
